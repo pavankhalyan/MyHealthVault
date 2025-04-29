4fb9d05c07de47207b4598b1fbaa94a24cc6496f
Sabari Doss R <sabursd18@gmail.com>
2025-04-29 08:39:19 +0530

    feat : Add Route for login and signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login.jsx";
+import Signup from './pages/Signup.jsx'
 
 function App() {
      
@@ -10,6 +11,7 @@ function App() {
       <Routes>
        <Route path="/" element={<Landing />} />
        <Route path="/login" element={<Login />} />
+       <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   )
