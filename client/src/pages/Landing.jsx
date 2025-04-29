d1def3a7dfea24318f8f7fbb7ce92826f575f647
Sabari Doss R <sabursd18@gmail.com>
2025-04-29 08:11:03 +0530

    feat: add initial landing page styles with background image and centered card

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,20 +1,25 @@
 import { Link } from "react-router-dom";
-
-function Landing(){
-    return (
-        <div>
-            <h1>Decentralized Healthcare Records</h1>
-            <p>Own, manage, and control your health data securely and easily.</p>
-         <div>
-            <Link>
-               <button to="/login" >Login</button>
-            </Link>
-            <Link>
-               <button to="/signup" >Signup</button>
-            </Link>
-         </div>
+import "../App.css";
+import LandingImage from '../assets/Healthcare.jpg';
+function Landing() {
+  return (
+    <div className="landing-container">
+      <div className="landing-card">
+        <h1 className="landing-title">Decentralized Healthcare Records</h1>
+        <p className="landing-description">
+          Own, manage, and control your health data securely and easily.
+        </p>
+        <div className="landing-buttons">
+          <Link to="/login">
+            <button className="btn btn-primary">Login</button>
+          </Link>
+          <Link to="/signup">
+            <button className="btn btn-outline">Signup</button>
+          </Link>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
